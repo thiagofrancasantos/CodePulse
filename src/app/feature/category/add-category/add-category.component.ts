249a0bb4c4c16ed2e059f4b8acdaf16f1d21bb1f
thiagofrancasantos <francasantos.t@gmail.com>
2025-06-26 12:09:15 -0400

    Service works

--- src/app/feature/category/add-category/add-category.component.ts
+++ src/app/feature/category/add-category/add-category.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
+import { CategoryService } from '../services/category.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-category',
   templateUrl: './add-category.component.html',
   styleUrls: ['./add-category.component.css']
 })
-export class AddCategoryComponent {
-  model: AddCategoryRequest;
 
-  constructor(){
+
+export class AddCategoryComponent implements OnDestroy{
+  model: AddCategoryRequest;
+  private addCategorySubscribtion?: Subscription;
+
+  constructor(private categoryService: CategoryService ){
     this.model = {
       name: '',
       urlHandle: ''
@@ -17,6 +22,18 @@ export class AddCategoryComponent {
   }
 
   onFormSubmit(){
-    console.log(this.model);
+    this.addCategorySubscribtion =  this.categoryService.addCategory(this.model)
+    .subscribe({
+      next: (response) => {
+        console.log('This was successful!');
+      },
+      error: (error) => {
+
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.addCategorySubscribtion?.unsubscribe();
   }
 }
