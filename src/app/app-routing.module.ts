b53b6189104a9a42676cd830398762d7b3f93da5
thiagofrancasantos <francasantos.t@gmail.com>
2025-07-02 08:05:15 -0400

    update category done

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CategoryListComponent } from './feature/category/category-list/category-list.component';
+import { AddCategoryComponent } from './feature/category/add-category/add-category.component';
+import { EditCategoryComponent } from './feature/category/edit-category/edit-category.component';
+
+const routes: Routes = [
+  {
+    path: 'admin/categories',
+    component: CategoryListComponent
+  },
+  {
+    path: 'admin/categories/add',
+    component: AddCategoryComponent
+  },
+  {
+    path: 'admin/categories/:id',
+    component: EditCategoryComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { CategoryListComponent } from './feature/category/category-list/category-list.component';
+import { AddCategoryComponent } from './feature/category/add-category/add-category.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { EditCategoryComponent } from './feature/category/edit-category/edit-category.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    CategoryListComponent,
+    AddCategoryComponent,
+    EditCategoryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
