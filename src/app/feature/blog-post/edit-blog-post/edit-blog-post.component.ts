b62159374c580331d45e4d9775c684a3da24e237
thiagofrancasantos <francasantos.t@gmail.com>
2025-07-16 14:36:41 -0400

    delete blogpost funcionando no front e no back, implementado no repository e controller

--- src/app/feature/blog-post/edit-blog-post/edit-blog-post.component.ts
+++ src/app/feature/blog-post/edit-blog-post/edit-blog-post.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
 import { BlogPostService } from '../services/blog-post.service';
 import { BlogPost } from '../model/blog-post.model';
 import { CategoryService } from '../../category/services/category.service';
 import { Category } from '../../category/models/category.model';
+import { UpdateBlogPost } from '../model/update-blog-post.model';
 
 @Component({
   selector: 'app-edit-blog-post',
@@ -17,12 +18,15 @@ export class EditBlogPostComponent implements OnInit, OnDestroy{
   categories$?: Observable<Category[]>;
   selectedCategories?: string[];
 
-
+  updateBlogPostSubscription?: Subscription;
   routeSubscription?: Subscription;
+  getBlogPostSubscription?: Subscription;
+  deleteBlogPostSubscription?: Subscription;
 
   constructor(private route: ActivatedRoute,
     private blogPostService: BlogPostService,
-    private categoryService: CategoryService
+    private categoryService: CategoryService,
+    private router: Router
   ){
 
   }
@@ -37,7 +41,7 @@ export class EditBlogPostComponent implements OnInit, OnDestroy{
 
         // Get BlogPost From API
         if(this.id){
-          this.blogPostService.getBlogPostById(this.id).subscribe({
+          this.getBlogPostSubscription = this.blogPostService.getBlogPostById(this.id).subscribe({
             next: (response) => {
               this.model = response;
               this.selectedCategories = response.categories.map(x => x.id);
@@ -48,11 +52,47 @@ export class EditBlogPostComponent implements OnInit, OnDestroy{
     })
   }
 
-  onFormSubmit(): void{}
+  onFormSubmit(): void{
+    // Convert this model to request object
+    if(this.model && this.id){
+      var updateBlogPost: UpdateBlogPost = {
+        author: this.model.author,
+        content: this.model.content,
+        shortDescription: this.model.shortDescription,
+        featuredImageUrl: this.model.featuredImageUrl,
+        isVisible: this.model.isVisible,
+        publishedDate: this.model.publishedDate,
+        title: this.model.title,
+        urlHandle: this.model.urlHandle,
+        categories: this.selectedCategories ?? []
+      };
 
+      this.updateBlogPostSubscription = this.blogPostService.updateBlogPost(this.id, updateBlogPost)
+      .subscribe({
+        next: (response) => {
+          this.router.navigateByUrl('/admin/blogposts');
+        }
+      })
+    }
+  }
+
+  onDelete(): void{
+    if(this.id){
+      // call service and delete blogPost
+      this.deleteBlogPostSubscription = this.blogPostService.deleteBlogPost(this.id)
+      .subscribe({
+        next: (response) => {
+          this.router.navigateByUrl('/admin/blogposts');
+        }
+      });
+    }
+  }
 
   ngOnDestroy(): void {
     this.routeSubscription?.unsubscribe();
+    this.updateBlogPostSubscription?.unsubscribe();
+    this.getBlogPostSubscription?.unsubscribe();
+    this.deleteBlogPostSubscription?.unsubscribe();
   }
 
 }
